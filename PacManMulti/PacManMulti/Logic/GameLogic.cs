0b07c26b4e1d471612e8599a83f0a49d89fd8101
CorruptingAcid <corruptingacid@gmail.com>
2014-08-13 15:41:33 -0600

    Added some classes for gameLogic
    Added an abstarct rules class and game logic

---
+++ PacManMulti/PacManMulti/Logic/GameLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PacManMulti.Logic
+{
+    class GameLogic
+    {
+        public void GameLogic(Rules m, int GhostHuman, int GhostAI, int PackPlayerHuman, int PackPlayerAI, Board b, BoardLayout bl)
+        {
+
+        }
+    }
+}

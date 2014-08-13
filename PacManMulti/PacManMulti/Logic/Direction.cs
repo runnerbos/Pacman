5e7f5ed8f355c254a648854990416188438873cb
CorruptingAcid <corruptingacid@gmail.com>
2014-08-13 09:17:38 -0600

    This addition adds in several logical representations of the elements in the game.
    The board is just an observable collection. This will allow the UI to be
    notified when elements are removed (e.g. a pacdot)
    Element is anything that might end up on the board
    element type is the specific type of what is on the board

---
+++ PacManMulti/PacManMulti/Logic/Direction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PacManMulti.Logic
+{
+    public enum Direction
+    {
+        Left, Right, Up, Down, Start, Neutral
+    }
+}
---
+++ PacManMulti/PacManMulti/Logic/ElementType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PacManMulti.Logic
+{
+    public enum ElementType
+    {
+        Ghost, PacPlayer, Wall, PacDot, Fruit, Powerup
+    }
+}

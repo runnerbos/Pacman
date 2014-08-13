36df0a8652ddf161619e0a80b3af7e163f0ec74f
runnerbos <runnerbos@gmail.com>
2014-08-13 15:59:59 -0600

    This is the first commit. This is a test.

---
+++ PacManMulti/PacManMulti/UI/View.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PacManMulti.UI
+{
+    abstract class View
+    {
+        // Fill in for all View elements to implement from
+    }
+}
